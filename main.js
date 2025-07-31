8c3f7b9280daaef0c6ea531e8f172a4b1e880c7c
kphanswims15 <phanlkim@comcast.net>
2025-07-30 22:53:54 -0600

    working on changing the colors that displays the gap between male and females

--- main.js
+++ main.js
@@ -103,20 +103,22 @@ function drawDetailScene(data, industry) {
       .call(d3.axisLeft(y));
   
     // Bars
-    svg.selectAll(".bar")
-      .data([
+    const genderData = [
         { group: "Men", value: selected.Male_Median },
         { group: "Women", value: selected.Female_Median }
-      ])
-      .enter()
-      .append("rect")
-      .attr("class", "bar")
-      .attr("x", d => x(d.group))
-      .attr("y", d => y(d.value))
-      .attr("width", x.bandwidth())
-      .attr("height", d => y(0) - y(d.value))
-      .attr("fill", d => d.group.trim() === "Men" ? "steelblue" : "pink");
-      
+      ];
+    
+    svg.selectAll(".bar")
+    .data(genderData)
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .attr("x", d => x(d.group))
+    .attr("y", d => y(d.value))
+    .attr("width", x.bandwidth())
+    .attr("height", d => y(0) - y(d.value))
+    .attr("fill", d => d.group === "Men" ? "steelblue" : "pink"); 
+
     // Title
     svg.append("text")
       .attr("x", width / 2)
