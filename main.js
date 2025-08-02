0a27639572161a1ffb68b357578e19775db30ed7
kphanswims15 <phanlkim@comcast.net>
2025-08-01 18:56:16 -0600

    Fixing the x axis to make the names more readable

--- main.js
+++ main.js
@@ -147,20 +147,20 @@ function drawDetailScene(data, industry) {
 function wrap(text, width) {
   text.each(function () {
     const text = d3.select(this),
-          words = text.text().split(/\s+/).reverse(),
-          lineHeight = 1.1; // ems
+          words = text.text().split(/\s+/).reverse();
     let word,
         line = [],
         lineNumber = 0,
+        lineHeight = 1.1, // ems
         y = text.attr("y"),
-        dy = parseFloat(text.attr("dy") || 0),
+        dy = parseFloat(text.attr("dy")) || 0,
         tspan = text.text(null)
                     .append("tspan")
                     .attr("x", 0)
                     .attr("y", y)
                     .attr("dy", dy + "em");
 
-    while (word = words.pop()) {
+    while ((word = words.pop())) {
       line.push(word);
       tspan.text(line.join(" "));
       if (tspan.node().getComputedTextLength() > width) {
